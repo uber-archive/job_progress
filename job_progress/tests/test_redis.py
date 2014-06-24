c3293dba65e5f36cace41dcf7503da7e3cdb6a4c
Caleb Mingle <mingle@uber.com>
2014-06-24 01:10:18 +0000

    Rename use_pipeline to using_twemproxy

--- job_progress/tests/test_redis.py
+++ job_progress/tests/test_redis.py
@@ -6,10 +6,10 @@ from job_progress.backends.redis import RedisBackend
 from job_progress.tests import TEST_SETTINGS
 
 
-def test_initialize_with_pipeline():
-    """Test that use_pipeline = True in the RedisBackend setting uses a pipeline."""
+def test_initialize_with_twemproxy():
+    """Test that using_twemproxy = True in the RedisBackend setting uses a pipeline."""
     settings = dict(TEST_SETTINGS)
-    settings['use_pipeline'] = True
+    settings['using_twemproxy'] = True
     redis_backend = RedisBackend(settings)
 
     redis_backend.client = mock.Mock()
@@ -20,10 +20,10 @@ def test_initialize_with_pipeline():
 
     assert fake_pipeline.execute.called == True
 
-def test_initialize_without_pipeline():
-    """Test that use_pipeline = False in the RedisBackend setting does not use a pipeline."""
+def test_initialize_without_twemproxy():
+    """Test that using_twemproxy = False in the RedisBackend setting does not use a pipeline."""
     settings = dict(TEST_SETTINGS)
-    settings['use_pipeline'] = False
+    settings['using_twemproxy'] = False
     redis_backend = RedisBackend(settings)
 
     redis_backend.client = mock.Mock()

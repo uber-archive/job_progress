4d8579550ae24e3f889f39911f178fbca372032b
Caleb Mingle <mingle@uber.com>
2015-07-03 00:04:52 +0000

    Log all redis commands

--- job_progress/backends/redis.py
+++ job_progress/backends/redis.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
+import logging
 import redis
-import warnings
 
 from job_progress import states
 from job_progress.cached_property import cached_property
@@ -11,6 +11,9 @@ DEFAULT_SETTINGS = {
     "heartbeat_expiration": 3600,  # in seconds
     "using_twemproxy": True,
 }
+
+
+log = logging.getLogger(__name__)
 
 
 class RedisBackend(object):
@@ -30,16 +33,22 @@ class RedisBackend(object):
 
         self.get_client = get_client
 
+    def call_redis(self, func, *args, **kwargs):
+        log.info({
+            'message': 'Updating Redis',
+            'function_name': func.__name__,
+            'args': args,
+            'kwargs': kwargs,
+        })
+
+        return func(*args, **kwargs)
+
     def update_settings(self, settings):
         """Update the settings.
 
         :param dict settings:
         """
         self.settings.update(settings)
-
-        if self.settings.get('using_twemproxy'):
-            warnings.warn('Moving jobs between states with Twemproxy'
-                          'is a non-atomic operation')
 
     @cached_property
     def client(self):
@@ -54,33 +63,24 @@ class RedisBackend(object):
         """Initialize and store a job."""
         key = self._get_key_for_job_id(id_)
 
-        using_twemproxy = self.settings.get('using_twemproxy')
-        client = self.client.pipeline() if not using_twemproxy else self.client
-
         if data:
-            client.hmset(self._get_metadata_key(key, "data"), data)
-        client.set(self._get_metadata_key(key, "amount"), amount)
-        client.set(self._get_metadata_key(key, "state"), state)
-        client.sadd(self._get_key_for_index("all"), key)
-        client.sadd(self._get_key_for_index("state", state), key)
-        if not using_twemproxy:
-            client.execute()
+            self.call_redis(self.client.hmset, self._get_metadata_key(key, "data"), data)
+
+        self.call_redis(self.client.set, self._get_metadata_key(key, "amount"), amount)
+        self.call_redis(self.client.set, self._get_metadata_key(key, "state"), state)
+        self.call_redis(self.client.sadd, self._get_key_for_index("all"), key)
+        self.call_redis(self.client.sadd, self._get_key_for_index("state", state), key)
 
     def delete_job(self, id_, state):
         """Delete a job based on id."""
         key = self._get_key_for_job_id(id_)
 
-        using_twemproxy = self.settings.get('using_twemproxy')
-        client = self.client.pipeline() if not using_twemproxy else self.client
-
-        client.delete(self._get_metadata_key(key, "data"))
-        client.delete(self._get_metadata_key(key, "amount"))
-        client.delete(self._get_metadata_key(key, "state"))
-        client.delete(self._get_metadata_key(key, "heartbeat"))
-        client.srem(self._get_key_for_index("all"), key)
-        client.srem(self._get_key_for_index("state", state), key)
-        if not using_twemproxy:
-            client.execute()
+        self.call_redis(self.client.delete, self._get_metadata_key(key, "data"))
+        self.call_redis(self.client.delete, self._get_metadata_key(key, "amount"))
+        self.call_redis(self.client.delete, self._get_metadata_key(key, "state"))
+        self.call_redis(self.client.delete, self._get_metadata_key(key, "heartbeat"))
+        self.call_redis(self.client.srem, self._get_key_for_index("all"), key)
+        self.call_redis(self.client.srem, self._get_key_for_index("state", state), key)
 
     def get_data(self, id_):
         """Return data for a given job."""
@@ -101,15 +101,24 @@ class RedisBackend(object):
     def add_one_progress_state(self, id_, state):
         """Add one unit state."""
         key = self._get_key_for_job_id(id_)
-        self.client.hincrby(self._get_metadata_key(key, "progress"),
-                            state, 1)
+
+        self.call_redis(
+            self.client.hincrby,
+            self._get_metadata_key(key, "progress"),
+            state,
+            1,
+        )
+
         self.update_hearbeat(key)
 
     def update_hearbeat(self, key):
         """Update the task's heartbeat."""
-        self.client.setex(self._get_metadata_key(key, "heartbeat"),
-                          self.settings["heartbeat_expiration"],
-                          1)
+        self.call_redis(
+            self.client.setex,
+            self._get_metadata_key(key, "heartbeat"),
+            self.settings["heartbeat_expiration"],
+            1,
+        )
 
     def get_progress(self, id_):
         """Return progress."""
@@ -134,7 +143,8 @@ class RedisBackend(object):
 
         # First set the state
         state_key = self._get_metadata_key(key, "state")
-        self.client.set(state_key, state)
+
+        self.call_redis(self.client.set, state_key, state)
 
         # The very last thing is updating the index
         self.update_state_index(key, previous_state, state)
@@ -146,15 +156,11 @@ class RedisBackend(object):
         new_state_key = self._get_key_for_index("state", new_state)
 
         if previous_state:
-            if not self.settings.get('using_twemproxy'):
-                # This is an atomic operation.
-                self.client.smove(previous_state_key, new_state_key, key)
-            else:
-                # This is not an atomic operation
-                self.client.srem(previous_state_key, key)
-                self.client.sadd(new_state_key, key)
+            # This is not an atomic operation
+            self.call_redis(self.client.srem, previous_state_key, key)
+            self.call_redis(self.client.sadd, new_state_key, key)
         else:
-            self.client.sadd(new_state_key, key)
+            self.call_redis(self.client.sadd, new_state_key, key)
 
     def is_staled(self, id_):
         """Return True if job at id_ is staled."""
